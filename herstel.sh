933e05216489e6d8a4c4288c7d09fb9e9d7719ce
arvindautar <126400973+arvindautar@users.noreply.github.com>
2025-11-21 22:42:32 +0100

    Delete herstel.sh

--- herstel.sh
+++
@@ -1,8 +0,0 @@
-tar -czf fontconfig-backup.tar.gz fontconfig-backup-2025*
-# verplaats naar nieuwe systeem
-tar -xzf fontconfig-backup.tar.gz -C ~/
-cp -r ~/fontconfig-backup-*/fontconfig ~/.config/
-cp -r ~/fontconfig-backup-*/fonts/.fonts ~/
-cp -r ~/fontconfig-backup-*/fonts/.local/share/fonts ~/.local/share/fonts
-sudo cp ~/fontconfig-backup-*/environment.backup /etc/environment
-fc-cache -fr
